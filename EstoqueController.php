9bcb43e440a032ed522e8dd981f9e4b99dc50ed4
Jumaalmeida9 <138945142+Jumaalmeida9@users.noreply.github.com>
2024-03-21 22:00:03 -0300

    aula21_03_24

---
+++ EstoqueController.php
@@ -0,0 +1,20 @@
+<?php
+    require_once 'Estoque.php';
+
+    class EstoqueController{
+
+        private $estoqueModel;
+
+        public function __construct(Estoque $estoqueModel) {
+            $this->estoqueModel = $estoqueModel;
+        }
+            public function ListarEstoques(){
+                return $this->estoqueModel->getAllEstoques();
+            
+
+        }
+    }
+
+
+
+?>
